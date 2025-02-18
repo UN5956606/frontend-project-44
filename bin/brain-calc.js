6cd38f7fa2e273430b8b8a86db38031a6b8c87e3
Dergachev <djorim1243@gmail.com>
2025-02-18 14:39:34 +0300

    Add calc, rework even

---
+++ bin/brain-calc.js
@@ -0,0 +1,62 @@
+import greetUser from '../src/cli.js';
+import readlineSync from 'readline-sync';
+import getRandomNum from '../src/randomNums.js';
+
+
+const name = greetUser();
+let isNotRight = false;
+
+const isRight = (answer, userAnswer) => {
+  const toInt = parseInt(userAnswer);
+  // решил приводить к int чтобы при написании буквы в консоль вместо NaN выводило букву
+  // знаю что использует больше памяти из-за этого
+  if (toInt !== answer) {
+    console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${answer}'.\nLet's try again, ${name}`);
+    return false;
+  };
+  return true;
+}; // проверка верное ли число
+
+const expression = () => {
+  const rngExpressionType = getRandomNum(2);
+  const randomNumOne = getRandomNum(25);
+  const randomNumTwo = getRandomNum(10);
+  let userAnswer;
+  let answer = 0;
+  switch (rngExpressionType) {
+    case 0:
+      answer = randomNumOne + randomNumTwo;
+      console.log(`Question: ${randomNumOne} + ${randomNumTwo}`);
+      userAnswer = readlineSync.question(`Your answer: `);
+      if (isRight(answer, userAnswer) === false) {
+        return isNotRight = true;
+      };
+      break;
+    case 1:
+      answer = randomNumOne * randomNumTwo;
+      console.log(`Question: ${randomNumOne} * ${randomNumTwo}`);
+      userAnswer = readlineSync.question(`Your answer: `);
+      if (isRight(answer, userAnswer) === false) {
+        return isNotRight = true;
+      };
+      break;
+    case 2:
+      answer = randomNumOne - randomNumTwo;
+      console.log(`Question: ${randomNumOne} - ${randomNumTwo}`);
+      userAnswer = readlineSync.question(`Your answer: `);
+      if (isRight(answer, userAnswer) === false) {
+        return isNotRight = true;
+      };
+      break;
+    default:
+      break;
+  };
+};
+console.log(`What is the result of the expression?`);
+for (let i = 0; i < 3; i += 1) {
+  if (isNotRight === true) break;
+  expression();
+};
+if (isNotRight === false) {
+  console.log(`Congratulations, ${name}`);
+};
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,6 @@
 import greetUser from '../src/cli.js';
 import readlineSync from 'readline-sync';
+import getRandomNum from '../src/randomNums.js';
 
 const isEven = (num) => {
   if (num % 2 === 0 & num !== 0) return 'yes';
@@ -12,7 +13,7 @@ console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
 //костыль, не знаю как правильно закончить весь код досрочно.
 let itDidWell = true;
 for (let i = 0; i < 3; i += 1) {
-  const num = Math.floor(Math.random() * 100);
+  const num = getRandomNum(100);
   const correctAnswer = isEven(num);
   console.log(`Is ${num} even? `);
   const userAnswer = readlineSync.question(`Your answer: `);
