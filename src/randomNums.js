267c6992cffc88ab897925be2361e03d4c9efa16
Dergachev <djorim1243@gmail.com>
2025-02-20 18:53:10 +0300

    add min value

--- src/randomNums.js
+++ src/randomNums.js
@@ -1,6 +1,7 @@
-const randomNum = (amount) => {
-  const num = Math.floor(Math.random() * amount);
-  return num;
+const randomNum = (max, min = 0) => {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 };
 
 export default randomNum;
