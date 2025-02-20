567c546353a94ac8c2fa043f98b9da46321e6150
Dergachev <djorim1243@gmail.com>
2025-02-20 19:15:43 +0300

    changed the test logic

--- src/answerCheck.js
+++ src/answerCheck.js
@@ -1,8 +1,6 @@
 const isWrongCheck = (answer, userAnswer, name) => {
   if (parseInt(userAnswer) !== parseInt(answer) & userAnswer !== answer) {
     console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${answer}'.\nLet's try again, ${name}`);
-    console.log(userAnswer);
-    console.log(answer);
     return true;
   };
   console.log('Correct!');
