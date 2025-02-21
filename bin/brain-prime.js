77956f947fcd91c599015aa42985b8960688518f
Dergachev <djorim1243@gmail.com>
2025-02-21 20:37:46 +0300

    brain-prime rework

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -4,26 +4,23 @@ import greetUser from '../src/cli.js';
 import getRandomNum from '../src/randomNums.js';
 import isWrongCheck from '../src/answerCheck.js';
 
-const isPrime = (num) => {
-  if (num < 2) {
-    return 'no';
-  } else if (num === 2 || num === 3) {
-    return 'yes';
-  } else if (num % 2 === 0) {
-    return 'no';
-  } else {
-    for (let j = 3; j <= Math.sqrt(num); j += 2) { // Исправлено на <=
-      if (num % j === 0) {
-        return 'no';
-      }
-    }
-  }
-  return 'yes';
-};
-
 const questionGeneration = () => {
   const randomNumber = getRandomNum(100, 1);
-  let answer = isPrime(randomNumber);
+  let answer = 'yes';
+  if (randomNumber < 2) {
+    answer = 'no';
+  }
+  if (randomNumber === 2 || randomNumber === 3) {
+    answer = 'yes';
+  }
+  if (randomNumber % 2 === 0) {
+    answer = 'no';
+  }
+  for (let j = 3; j <= Math.sqrt(randomNumber); j += 2) {
+    if (randomNumber % j === 0) {
+      answer = 'no';
+    }
+  }
   console.log(`Question: ${randomNumber}`);
   isNotRight = isWrongCheck(answer, name);
 };
