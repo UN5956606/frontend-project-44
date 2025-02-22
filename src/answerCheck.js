88b6b4f2fc8ebb6a3291c05a1a2c1aa9a7affb78
Dergachev <djorim1243@gmail.com>
2025-02-22 22:40:33 +0300

    Changed the inputs to the Answer Check

--- src/answerCheck.js
+++ src/answerCheck.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const isWrongCheck = (answer, name) => {
+const isWrongCheck = (asnwerAndQuestion, name) => {
+  console.log(asnwerAndQuestion[1]);
   const userAnswer = readlineSync.question(`Your answer: `);
-  if (parseInt(userAnswer) !== parseInt(answer) & userAnswer !== answer) {
-    console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${answer}'.\nLet's try again, ${name}`);
+  if (parseInt(userAnswer) !== parseInt(asnwerAndQuestion[0]) & userAnswer !== asnwerAndQuestion[0]) {
+    console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${asnwerAndQuestion[0]}'.\nLet's try again, ${name}`);
     return true;
   };
   console.log('Correct!');
