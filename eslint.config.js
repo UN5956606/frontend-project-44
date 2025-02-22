2a415ccfb8de965a2cd12f9fceee3e4822e294b2
Dergachev <djorim1243@gmail.com>
2025-02-23 02:28:55 +0300

    changed rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,43 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
 
-/** @type {import('eslint').Linter.Config[]} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
+
 export default [
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+
+      parserOptions: {
+        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+
+    },
+  },
+  {
+    rules: {
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/extensions': 'off',
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+    },
+  },
 ];
