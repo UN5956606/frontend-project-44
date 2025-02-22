e719179810b95ab2dd814686de793ed2d8e3315d
Dergachev <djorim1243@gmail.com>
2025-02-23 02:23:20 +0300

    Fixed linter

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,23 +4,21 @@ import getRandomNum from '../randomNums.js';
 import defaultLogic from '../engine.js';
 
 const gcd = () => {
-  const answer = (randomNumberOne, randomNumberTwo) => {
-    if (randomNumberOne === 0) return randomNumberTwo;
-    if (randomNumberTwo === 0) return randomNumberOne;
-    return gcd(randomNumberOne, randomNumberTwo);
-  };
-
-  const gcd = (num1, num2) => {
+  const whatGcdOf = (num1, num2) => {
     while (num1 !== 0 && num2 !== 0) {
       if (num1 > num2) {
-        num1 = num1 % num2;
+        num1 %= num2;
       } else {
-        num2 = num2 % num1;
+        num2 %= num1;
       }
     }
     return (num1 + num2);
   };
-
+  const answer = (randomNumberOne, randomNumberTwo) => {
+    if (randomNumberOne === 0) return randomNumberTwo;
+    if (randomNumberTwo === 0) return randomNumberOne;
+    return whatGcdOf(randomNumberOne, randomNumberTwo);
+  };
   const question = 'Find the greatest common divisor of given numbers.';
   const gameQuestionGenerator = () => {
     const randomNumberOne = getRandomNum(100);
