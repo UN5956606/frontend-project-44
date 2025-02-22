efc359f676dd131cfed692754288551114f4f5b1
Dergachev <djorim1243@gmail.com>
2025-02-23 02:33:47 +0300

    fixed 0 not being an even num

--- src/games/even.js
+++ src/games/even.js
@@ -5,12 +5,12 @@ import getRandomNum from '../randomNums.js';
 
 const even = () => {
   const isEven = (num) => {
-    if (num % 2 === 0 & num !== 0) return 'yes';
+    if (num % 2 === 0) return 'yes';
     return 'no';
   };
   const question = `Answer "yes" if the number is even, otherwise answer "no".`;
   const gameQuestionGenerator = () => {
-    const num = getRandomNum(100);
+    const num = getRandomNum(512);
     const correctAnswer = isEven(num);
     const expression = `Question: ${num}`;
     return [correctAnswer, expression];
