27b65f45240b3f6bf1389ce0d098e8a7f4dc2ddc
Dergachev <djorim1243@gmail.com>
2025-02-22 23:32:23 +0300

    added !

--- src/answerCheck.js
+++ src/answerCheck.js
@@ -4,7 +4,7 @@ const isWrongCheck = (asnwerAndQuestion, name) => {
   console.log(asnwerAndQuestion[1]);
   const userAnswer = readlineSync.question(`Your answer: `);
   if (parseInt(userAnswer) !== parseInt(asnwerAndQuestion[0]) & userAnswer !== asnwerAndQuestion[0]) {
-    console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${asnwerAndQuestion[0]}'.\nLet's try again, ${name}`);
+    console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${asnwerAndQuestion[0]}'.\nLet's try again, ${name}!`);
     return true;
   };
   console.log('Correct!');
