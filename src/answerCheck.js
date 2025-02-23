1057187a7786a5fe32e3a1751cd681f2683f3f02
Dergachev <djorim1243@gmail.com>
2025-02-23 03:07:36 +0300

    fixed linter

--- src/answerCheck.js
+++ src/answerCheck.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const isWrongCheck = (asnAndQuestion, name) => {
   console.log(asnAndQuestion[1]);
-  const answer = asnAndQuestion[0]; //для исправления ошибки линтера
+  const answer = asnAndQuestion[0]; // для исправления ошибки линтера
   const userAnswer = readlineSync.question('Your answer: ');
   if (parseInt(userAnswer, 10) !== parseInt(answer, 10) && userAnswer !== answer) {
     console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${asnAndQuestion[0]}'.\nLet's try again, ${name}!`);
