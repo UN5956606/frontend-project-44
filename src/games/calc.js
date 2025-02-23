aca72c95165da3b56c0915527f7b24a51899ff47
Dergachev <djorim1243@gmail.com>
2025-02-23 14:52:10 +0300

    Changed the way data is stored

--- src/games/calc.js
+++ src/games/calc.js
@@ -8,25 +8,24 @@ const calc = () => {
   const expressionGenerator = () => {
     const rngExpressionType = getRandomNum(2); // выбор типа выражения 0-2 (+*-)
     const randomNumbers = [getRandomNum(25), getRandomNum(10)];
-    let answer = 0;
-    let question = '';
+    const asnAndQue = [];
     switch (rngExpressionType) {
       case 0:
-        answer = randomNumbers[0] + randomNumbers[1];
-        question = `Question: ${randomNumbers[0]} + ${randomNumbers[1]}`;
+        asnAndQue[0] = randomNumbers[0] + randomNumbers[1];
+        asnAndQue[1] = `Question: ${randomNumbers[0]} + ${randomNumbers[1]}`;
         break;
       case 1:
-        answer = randomNumbers[0] * randomNumbers[1];
-        question = `Question: ${randomNumbers[0]} * ${randomNumbers[1]}`;
+        asnAndQue[0] = randomNumbers[0] * randomNumbers[1];
+        asnAndQue[1] = `Question: ${randomNumbers[0]} * ${randomNumbers[1]}`;
         break;
       case 2:
-        answer = randomNumbers[0] - randomNumbers[1];
-        question = (`Question: ${randomNumbers[0]} - ${randomNumbers[1]}`);
+        asnAndQue[0] = randomNumbers[0] - randomNumbers[1];
+        asnAndQue[1] = (`Question: ${randomNumbers[0]} - ${randomNumbers[1]}`);
         break;
       default:
         break;
     }
-    return [answer, question];
+    return asnAndQue;
   };
   const question = 'What is the result of the expression?';
   defaultLogic(expressionGenerator, question);
