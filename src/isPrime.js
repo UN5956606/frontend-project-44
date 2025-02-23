44c60b832a5ba36db2c2d3004a86280715458f87
Dergachev <djorim1243@gmail.com>
2025-02-23 21:35:08 +0300

    isPrime fucntion rework

--- src/isPrime.js
+++ src/isPrime.js
@@ -4,13 +4,13 @@ const isNumPrime = (randomNumber) => {
   }
   if (randomNumber === 2 || randomNumber === 3) {
     return 'yes';
-  } else if (randomNumber % 2 === 0) {
+  }
+  if (randomNumber % 2 === 0) {
     return 'no';
   } else {
     for (let j = 3; j <= Math.sqrt(randomNumber); j += 2) {
       if (randomNumber % j === 0) {
         return 'no';
-        break;
       }
     }
   }
