d9a241f1a5d9216eaed02a403c81c8032e9a9295
Dergachev <djorim1243@gmail.com>
2025-02-23 21:38:41 +0300

    linter fix

--- src/isPrime.js
+++ src/isPrime.js
@@ -7,13 +7,12 @@ const isNumPrime = (randomNumber) => {
   }
   if (randomNumber % 2 === 0) {
     return 'no';
-  } else {
+  }
     for (let j = 3; j <= Math.sqrt(randomNumber); j += 2) {
       if (randomNumber % j === 0) {
         return 'no';
       }
     }
-  }
   return 'yes';
 };
 
